817d8584745908956b7132085a32a9cc64608a9a
Kyle Moore <kbm190@icloud.com>
2022-04-09 10:12:54 -0500

    add new button

--- popup.js
+++ popup.js
@@ -1,20 +1,9 @@
-let changeColor = document.getElementById("changeColor");
+let addNew = document.getElementById("addNew");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+addNew.addEventListener("click",() => {
+  console.log('add new');
+})
 
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
-
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
+// chrome.storage.sync.get("links", ({ color }) => {
+//   addNew.style.backgroundColor = color;
+// });
