473bb89b060a8a6792a13b22801b07348b63c1f9
Kyle Moore <kbm190@icloud.com>
2022-04-09 11:19:11 -0500

    render data

---
+++ background.js
@@ -0,0 +1,15 @@
+let test = [{
+    id: 'ojgfdsk',
+    link: 'https://google.com',
+    description: "this is google",
+    todos: ['check out google', 'testing']
+},{
+    id: 'ojgffdsdsk',
+    link: 'https://googlfsde.com',
+    description: "thfdsis is google",
+    todos: ['check out gofdsogle', 'testing']
+}]
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ links: JSON.stringify(test) });
+  });
--- popup.js
+++ popup.js
@@ -1,9 +1,47 @@
 let addNew = document.getElementById("addNew");
+let linkContainer = document.getElementById("linkContainer");
 
-addNew.addEventListener("click",() => {
+function renderLinks() {
+  chrome.storage.sync.get("links", ({ links }) => {
+    let linksParsed = JSON.parse(links);
+
+    console.log(linksParsed)
+
+    for (let i = 0; i < linksParsed.length; i++) {
+
+      console.log(i, linksParsed[i])
+      const node = document.createElement("div");
+
+      const attribute = document.createElement("a");
+      attribute.innerText = linksParsed[i].link;
+      node.appendChild(attribute);
+
+      const description = document.createElement("p");
+      description.innerText = linksParsed[i].description;
+      node.appendChild(description);
+
+      const todoList = document.createElement("ul");
+      
+      for (let j = 0; j < linksParsed[i].todos.length; j++) {
+        const todoDescription = document.createElement("li");
+        todoDescription.innerText = linksParsed[i].todos[j];
+
+        todoList.appendChild(todoDescription);
+
+      
+      }
+ 
+      node.appendChild(todoList);
+
+
+      linkContainer.appendChild(node);
+    }
+  });
+}
+
+addNew.addEventListener("click", () => {
   console.log('add new');
 })
 
-// chrome.storage.sync.get("links", ({ color }) => {
-//   addNew.style.backgroundColor = color;
-// });
+
+renderLinks()
